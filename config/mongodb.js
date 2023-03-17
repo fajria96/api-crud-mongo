f566fcebdb5c80c7a6dab4b162bf6d39cf4c02f8
Fajriatun Munawaroh <fajriatun072@gmail.com>
2023-03-17 20:17:30 +0700

    fix error deploy

--- config/mongodb.js
+++ config/mongodb.js
@@ -1,6 +1,6 @@
 const { MongoClient } = require("mongodb");
 
-const url = "mongodb://fajriatun:fajriatun@localhost:27017?authSource=admin";
+const url =  "mongodb://fajriatun:hHIQ7azDPSh7yufM@ac-9xvgiaf-shard-00-00.dew2u1j.mongodb.net:27017,ac-9xvgiaf-shard-00-01.dew2u1j.mongodb.net:27017,ac-9xvgiaf-shard-00-02.dew2u1j.mongodb.net:27017/eduwork?ssl=true&replicaSet=atlas-mdm299-shard-0&authSource=admin&retryWrites=true&w=majority";;
 const client = new MongoClient(url);
 
 (async () => {
